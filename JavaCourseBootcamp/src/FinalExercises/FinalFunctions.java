73818b9fb5403a702a286564ecc88144036b8336
Diana Choubaev <diana.shub2@gmail.com>
2021-10-31 12:21:46 +0200

    added html and some more exercises in final exercises

--- JavaCourseBootcamp/src/FinalExercises/FinalFunctions.java
+++ JavaCourseBootcamp/src/FinalExercises/FinalFunctions.java
@@ -1,4 +1,7 @@
 package FinalExercises;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
+import java.util.ArrayList;
 
 public class FinalFunctions {
 
@@ -42,6 +45,94 @@ public class FinalFunctions {
     }
 
     // Ex 5
+    public void printCalcResult(char[] toCalculate){
 
+        switch (toCalculate[1]){
+            case '+':
+                System.out.println(Character.getNumericValue(toCalculate[0]) + Character.getNumericValue(toCalculate[2]));
+                break;
+            case '-':
+                System.out.println(Character.getNumericValue(toCalculate[0]) - Character.getNumericValue(toCalculate[2]));
+                break;
+            case '/':
+                if(Character.getNumericValue(toCalculate[2]) == 0){
+                    System.out.println("Cannot divide by 0.");
+                    break;
+                }
+                System.out.println(Character.getNumericValue(toCalculate[0]) / Double.valueOf(Character.getNumericValue(toCalculate[2])));
+                break;
+            case '*':
+                System.out.println(Character.getNumericValue(toCalculate[0]) * Character.getNumericValue(toCalculate[2]));
+                break;
+        }
+
+    }
+
+    // Ex 6
+    public void newListEvenandDevidedBy4(int[] nums){
+        ArrayList newArr = new ArrayList();
+
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i] % 2 == 0 && nums[i] % 4 == 0)
+                newArr.add(nums[i]);
+        }
+        System.out.println("The even numbers and can be divided by 4 are: " + newArr);
+    }
+
+    // Ex 7
+    public void getMedian(int[] nums){
+        if (nums.length % 2 == 1)
+            System.out.println("The median of the array is: " + nums[nums.length / 2 ]);
+        else
+            System.out.println("The median of the array is: " + (nums[nums.length / 2 - 1] + nums[nums.length / 2 ]) / 2);
+    }
+
+    // Ex 8
+    public void printSongWordsRandomized(String song){
+        String[] songWords = song.split(" ");
+        int randomNum = ThreadLocalRandom.current().nextInt(0, songWords.length);
+
+        for(int i=0; i<songWords.length; i++){
+            System.out.print(songWords[randomNum] + " ");
+            randomNum = ThreadLocalRandom.current().nextInt(0, songWords.length);
+        }
+    }
+
+    // Ex 9
+    public String getLongestString(String[] words){
+        String longestStr = "";
+
+        for(int i=0; i< words.length; i++){
+            if(words[i].length() > longestStr.length())
+                longestStr = words[i];
+        }
+        return longestStr;
+    }
+
+    public ArrayList getStringsThatContainASmallerString(String[] words, String strToSearch){
+
+        ArrayList wordsThatContainTheStr = new ArrayList();
+
+        for(int i = 0; i< words.length; i++){
+            if(words[i].contains(strToSearch))
+                wordsThatContainTheStr.add(words[i]);
+        }
+        return wordsThatContainTheStr;
+    }
+
+    // Ex 10
+    public void getNameAndGrade(String[][] nameGrade){
+        int maxGrade = 0, indexOfMax = 0;
+        for (int i= 0; i< nameGrade[1].length; i++){
+            if(Integer.valueOf(nameGrade[0][i]) > maxGrade) {
+                maxGrade = Integer.valueOf(nameGrade[0][i]);
+                indexOfMax = i;
+            }
+        }
+        if(maxGrade == 0)
+            System.out.println("No data");
+        else
+            System.out.println(nameGrade[1][indexOfMax] + " " + nameGrade[0][indexOfMax]);
+    }
 }
 
--- JavaCourseBootcamp/src/FinalExercises/FinalMain.java
+++ JavaCourseBootcamp/src/FinalExercises/FinalMain.java
@@ -4,7 +4,22 @@ public class FinalMain {
     public static void main(String[] args) {
 
         FinalFunctions Ex = new FinalFunctions();
-        int[] arr = {4, 21, 5, -3};
+        int[] arr = {4, 21, 5, -3, 16, 2};
+        char[] toCalculate = {'3', '/', '2'};
+        String song = "It might seem crazy what I am 'bout to say";
+        String[] listOfWords = {"ABC", "AAAAA", "BBB", "kjhdsfkjo", "JDSDFHKJH", "FGH", "LXCVKJ", "XLCKVJL",
+                            "VCKJLK", "VLK;LK", "VLKL"};
+        String[][] namesGrades = new String[2][5];
+        namesGrades[0][0] = "85";
+        namesGrades[0][1] = "76";
+        namesGrades[0][2] = "45";
+        namesGrades[0][3] = "91";
+        namesGrades[0][4] = "82";
+        namesGrades[1][0] = "David";
+        namesGrades[1][1] = "Moshe";
+        namesGrades[1][2] = "Ilana";
+        namesGrades[1][3] = "Shlomo";
+        namesGrades[1][4] = "Hanna";
 
         // Ex 1
         System.out.println(Ex.reverseNumber(12345));
@@ -15,6 +30,19 @@ public class FinalMain {
         Ex.printBetween1and10AfterDuplicate(arr);
         // Ex 4
         Ex.printAVG(arr);
-
+        // Ex 5
+        Ex.printCalcResult(toCalculate);
+        //Ex 6
+        Ex.newListEvenandDevidedBy4(arr);
+        // Ex 7
+        Ex.getMedian(arr);
+        // Ex 8 - After some reading I got to a conclusion seed wise, ThreadLocalRandom is better to use then Random
+        Ex.printSongWordsRandomized(song);
+        System.out.println();
+        // Ex 9
+        System.out.println("Longest string is: " + Ex.getLongestString(listOfWords));
+        System.out.println("String that contain the desirable string is: " + Ex.getStringsThatContainASmallerString(listOfWords, "CK"));
+        // Ex 10
+        Ex.getNameAndGrade(namesGrades);
     }
 }
